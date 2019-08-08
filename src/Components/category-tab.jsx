8e0a2014bab53cfec963abeb619b213c6d0ef325
Dimitris Tsiktsiris <ditsikts@outlook.com>
2019-08-08 18:47:48 +0300

    small fix

--- src/Components/category-tab.jsx
+++ src/Components/category-tab.jsx
@@ -17,11 +17,13 @@ class CategoryTab extends React.Component {
             style = {};
         }
 
+
+
         return (
             <div className="col-md-2 p-1 " key={this.props.cat.category} onClick={(e) => this.categorySelected(this.props.cat.category, e)}>
-                <div style={style} className="d-flex justify-content-between align-items-center p-1 rounded border border-success">
-                    <p>{this.props.cat.category}</p>
-                    <p className="badge badge-primary badge-pill">{this.props.cat.count}</p>
+                <div style={style} className="d-flex justify-content-between align-items-center p-2 rounded border border-success">
+                    <p className="mb-0">{this.props.cat.category}</p>
+                    <span className="badge badge-primary badge-pill">{this.props.cat.count}</span>
                 </div>
             </div>
         );
